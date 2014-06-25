72d21ff4341f858a926f9d72ab369ad0c125b3df
Rahul Mani <rahuladityamani@hotmail.com>
2014-06-25 13:19:16 -0700

    Interactive Story Final
    Interactive Story repoed from Evan Burchard's original and
    edited/modified by myself along with partners SindhuMajeti and Jananni

---
+++ quiz/final/game.js
@@ -0,0 +1,50 @@
+if(jQuery){
+  var checkAnswers = function(){
+    var answerString = "";
+    var answers = $(":checked");
+    answers.each(function(i) {
+      answerString = answerString + answers[i].value;
+    });
+    // See errata for chapter 1 at: github.com/EvanBurchard/jsarcade/blob/master/errata/chapter1 
+    // The 3 following lines with comments (preceded by //) are not necessary
+    // $(":checked").each(function(i) {
+    //   var answerString = answerString + answers[i].value;
+    //  });
+    checkIfCorrect(answerString);
+  };
+  var checkIfCorrect = function(theString){
+    if(parseInt(theString, 16) === 811124566973){
+      $("body").addClass("correct");
+      $("h1").text("You Win!");
+      $("canvas").show();
+    }
+  };
+  $("#question1").show();
+};
+if(impress){
+  $("#question2").show();
+};
+if(atom){
+  $("#question3").show();
+};
+if(createjs){
+  $("#question4").show();
+};
+if(me){
+  $("#question5").show();
+};
+if(require){
+  $("#question6").show();
+};
+if($().playground){
+  $("#question7").show();
+};
+if(jaws){
+  $("#question8").show();
+};
+if(enchant){
+  $("#question9").show();
+};
+if(Crafty){
+  $("#question10").show();
+};
